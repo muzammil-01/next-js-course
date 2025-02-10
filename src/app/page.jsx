b1718675e405f320c9840fad6aecc8a3f37c1d91
muzammilmuhammad <muhammad.muzammil@xpertdigital.co>
2025-02-10 12:14:05 +0500

    header component

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <center><div >
+    Home Page
+    </div></center>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <main>
+            <h2>Logo</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </main>
+      </header>
+    </div>
+  )
+}
